f2511e37a87396d864e339608067a5f6eeb3f38d
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-07-07 03:47:17 +0100

    Made design responsive4

--- src/components/modals/BlockedSuccessfulModal.tsx
+++ src/components/modals/BlockedSuccessfulModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 interface ModalProps{
     children?:React.ReactNode,
@@ -8,7 +8,23 @@ interface ModalProps{
 
 
 
+
 export default function BlockedSuccessfulModal({children,customStyle}:ModalProps){
+
+    const [visible, setVisible] = useState(true);
+
+
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setVisible(false);
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    if (!visible) return null;
+
     return(
         <>
             <div className={`blocked-successful-modal ${customStyle ?? ""}`}>
